70748c6419b5d0acd12d28b4b360aabe9398f2f5
verywelloo <verywelloo@hotmail.com>
2023-12-02 10:24:57 +0700

    update

--- 5-JWT-Basics(2)/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken')
-const {UnauthenticatedError} = require('../errors')
-
-const authenticationMiddleware = async(req, res, next) => {
-    const authHeader = req.headers.authorization;
-}
